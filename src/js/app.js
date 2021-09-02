b3345fe72ae1a2ace10843f31d2870b188c12b9a
Maria <mariaes2016@yandex.ru>
2021-09-02 11:34:47 +0300

    symbols-generate

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+import Bowman from './Bowman';
+import Deamon from './Deamon';
+import Magician from './Magician';
+import Swordsman from './Swordsman';
+import Team from './Team';
+
+const group1 = new Team();
+const player1 = new Bowman('Alex');
+const player2 = new Deamon('Ivan');
+const player3 = new Magician('Olga');
+const player4 = new Swordsman('Jack');
+
+group1.addAll([player1, player2, player3, player4]);
+
+for (const player of group1) {
+  // eslint-disable-next-line no-console
+  console.log(player);
+}
